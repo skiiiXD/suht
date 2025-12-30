de3fa9b58cc74f77866d03c8e4abe502beda9b2b
skiiiXD <pomogitehue@gmail.com>
2025-12-30 20:54:16 +0300

    renames

--- suht-update.bat
+++ suht-update.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 curl -L -o "suht.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/suht.bat"
-curl -L -o "suht-downoload.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/downoload.bat"
-curl -L -o "suht-update.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/update.bat"
+curl -L -o "suht-downoload.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/suht-downoload.bat"
+curl -L -o "suht-update.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/suht-update.bat"
 suht-downoload.bat
