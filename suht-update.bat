162046ca7cf7fd21a3ebb22faa66c78296e31c79
skiiiXD <pomogitehue@gmail.com>
2025-12-31 11:04:50 +0300

    updated suht-update.bat

--- suht-update.bat
+++ suht-update.bat
@@ -1,6 +1,12 @@
 @echo off
 cd /d "%~dp0"
+set /A num=2
+:upd
+set /A num=%num%-1
 curl -L -o "suht.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/suht.bat"
 curl -L -o "suht-downoload.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/suht-downoload.bat"
 curl -L -o "suht-update.bat" "https://raw.githubusercontent.com/skiiiXD/suht/refs/heads/main/suht-update.bat"
+if %num%==1 (
+    goto :upd
+)
 suht-downoload.bat
